f8a49e8a0ac0cbf3a52531adedcfdadd507f539c
Dimiter 'malkia' Stanev <malkia@gmail.com>
2026-02-19 11:32:40 -0800

    Enable deprecated functions, as grpc can't compile without them yet

--- include/ares_build.h
+++ include/ares_build.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#ifdef CARES_NO_DEPRECATED
+#error CARES_NO_DEPRECATED can't be defined outside this header file
+#undef CARES_NO_DEPRECATED
+#endif
+
+// malkia: grpc still calls ares_gethostbyname so we can't deprecate it yet
+#define CARES_NO_DEPRECATED 1
+
 #if defined(_WIN32)
 #include "ares_build_windows.h"
 #endif
